6a2df99fd620a10a40f114e5d938ee3e5211fffd
Ubuntu <ubuntu@ip-172-31-45-75.us-west-2.compute.internal>
2019-10-22 05:41:34 +0000

    updated error response

--- plotbot/service/sampler.py
+++ plotbot/service/sampler.py
@@ -1,17 +1,18 @@
 def retrieve_snippet(type):
     snippet_dict={"scatterplot": ["scatterplot_code","scatterplot_graph"],
-         "barplot": ["barplot_code","barplot_graph"], "boxplot": ["boxplot_code","boxplot_graph"]} 
-    return snippet_dict[type]
+         "barplot": ["barplot_code","barplot_graph"], "boxplot": ["boxplot_code","boxplot_graph"]}
+    if type in snippet_dict:
+        return snippet_dict[type]
+    else:
+        raise ValueError('Requested plot sample not available')
 
 def fetch(input_txt):
     text_list = input_txt.lower().strip().split()
     files=[]
 
     snippets=retrieve_snippet(text_list[1])
-    if snippets:
-        for name in snippets:
-            path="sample_plots/"+name+".png"
-            files.append(path)
-        return "Here is you code snippet for **{}**".format(text_list[1]),files
-    else:
-        raise ValueError('A very specific bad thing happened')
+    for name in snippets:
+        path="sample_plots/"+name+".png"
+        files.append(path)
+    return "Here is you code snippet for **{}**".format(text_list[1]),files
+    
