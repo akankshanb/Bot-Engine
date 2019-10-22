5443632db86869a997b8600b0bad82a97896e5ef
Mounika Nekkalapudi <snekkal@ncsu.edu>
2019-10-21 20:16:33 -0400

    selenium test cases

---
+++ plotbot/selenium/mattermost.js
@@ -0,0 +1,54 @@
+const puppeteer = require('puppeteer')
+
+const loginEmail = process.env.MATTERMOST_EMAIL;
+const loginPassword = process.env.MATTERMOST_PWD;
+const mattermostUrl = 'http://ec2-18-217-150-234.us-east-2.compute.amazonaws.com:8065/plotbotteam/channels/mounika_trials'
+
+async function login(browser, url) {
+  const page = await browser.newPage();
+
+  await page.goto(url, {waitUntil: 'networkidle0'});
+
+  // Login
+  await page.type('input[id=loginId]', loginEmail);
+  await page.type('input[id=loginPassword]', loginPassword);
+  await page.click('button[id=loginButton]');
+
+  // Wait for redirect
+  await page.waitForNavigation();
+  return page;
+}
+
+async function postMessage(page, msg)
+{
+  // Waiting for page to load
+  await page.waitForSelector('#post_textbox');
+
+  // Focus on post textbox and press enter.
+  await page.focus('#post_textbox')
+  await page.keyboard.type( msg );
+  await page.keyboard.press('Enter');
+  await page.waitForSelector('#post_textbox');
+
+  const tweets = await page.evaluate(() => Array.from(document.getElementsByClassName('post__body'), e => e.innerText));
+
+var len = tweets.length
+console.log(tweets[len-2]);
+console.log("***************************");
+
+tweets.forEach(tweet => {
+  console.log(tweet);
+  console.log("-----------------")
+});
+}
+
+
+(async () => {
+
+  const browser = await puppeteer.launch({headless: false, args: ["--no-sandbox", "--disable-web-security"]});
+  let page = await login( browser, `${mattermostUrl}/login` );
+  await postMessage(page, "@plotbot hi" );
+
+  // const html = await page.content(); // serialized HTML of page DOM.
+  // browser.close();
+})()
---
+++ plotbot/selenium/test/mattermost.js
@@ -0,0 +1,57 @@
+const puppeteer = require('puppeteer');
+const { expect }  = require('chai');
+
+const loginEmail = process.env.MATTERMOST_EMAIL;
+const loginPassword = process.env.MATTERMOST_PWD;
+
+async function login(browser, url) {
+  const page = await browser.newPage();
+
+  await page.goto(url, {waitUntil: 'networkidle0'});
+
+  // Login
+  await page.type('input[id=loginId]', loginEmail);
+  await page.type('input[id=loginPassword]', loginPassword);
+  await page.click('button[id=loginButton]');
+
+  // Wait for redirect
+  await page.waitForNavigation();
+  return page;
+}
+
+
+describe('Testing PlotBot usecases', function () {
+
+    let browser;
+    let page;
+    let mattermost_url;
+
+    this.timeout(5000000);
+
+    beforeEach(async () => {
+        browser = await puppeteer.launch({headless:false});
+        mattermost_url = "http://ec2-18-217-150-234.us-east-2.compute.amazonaws.com:8065/plotbotteam/channels/utsav_trialas";
+        page = await login( browser, `${mattermost_url}/login` );
+        //await page.goto(`${mattermost_url}`, {waitUntil: 'networkidle0'});
+    });
+
+    //afterEach(async () => {
+      //  await browser.close();
+    //});
+
+    it('It should greet back', async () => {
+
+        await page.waitForSelector('#post_textbox');
+        await page.focus('#post_textbox')
+        await page.keyboard.type( "@plotbot hi" );
+        await page.keyboard.press('Enter');
+        //await page.waitForSelector('h2 a');
+        const output = await page.evaluate(() => Array.from(document.getElementsByClassName('post__body'), e => e.innerText));
+        console.log(output[output.length-2]);
+        var result = output[output.length-2];
+        expect(result).to.match(/^Hi*/);
+
+        await browser.close();
+    });
+
+});
