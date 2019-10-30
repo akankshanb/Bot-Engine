6b088b067d116c9c8f7b15ac320850352d08f6a5
Mounika Nekkalapudi <snekkal@ncsu.edu>
2019-10-29 20:25:15 -0400

    Update parser with few checks

--- plotbot/framework/parser.py
+++ plotbot/framework/parser.py
@@ -1,11 +1,44 @@
-import re
+# Message Sanitization
+from os import path
 import pandas as pd
-import csv
 
-# Assuming input to be x and y
+input = "plot scatterplot dataset.csv xaxis yaxis"
+input = input.lower().split(" ")
+plots = ["scatterplot","barplot","boxplot"]
 
-df = pd.read_csv("dataset.csv")
-x_axis = [x_val for x_val in df[x]]
-y_axis = [y_val for y_val in df[y]]
+def check_params(input):
+    input = input.split(" ")
+    if (len(input) < 4):
+        raise ValueError("Please provide all the required parameters for plotting graph.")
+    elif (len(input) >= 6):
+        raise ValueError("Please provide only the required parameters in the proper format.")
+    else:
+        if input[1] not in plots:
+                raise ValueError("Please provide correct plot type.")
+        else:
+            return True
+
+#check the uploaded file
+def check_axis(input,loc):
+    input = input.lower().split(" ")
+    df = pd.read_csv(loc)
+    headers = list(df.head(1))
+    headers = [each.lower() for each in headers]
+    x_axis = input[4].lower()
+    y_axis = input[4].lower()
+    if x_axis not in headers:
+        raise ValueError("Provided column for x-axis is not present in the dataset.Please provide valid column.")
+    if y_axis not in headers:
+        raise ValueError("Provided column for y-axis is not present in the dataset.Please provide valid column.")
+    return True 
 
 
+def check_plot_params(input):
+    input = input.lower().split(" ")
+    if (len_input == 5):
+        file = input[2]
+        loc = '../tmp/'+file
+        if (path.exists(loc)):
+            check_axis(input,loc)
+        else:
+            raise ValueError("Please request by uploading the dataset. The mentioned dataset doesn't exist in our databse.")
