162d4a46b14c832fb48468414dd762ee015f1d2e
Mounika Nekkalapudi <snekkal@ncsu.edu>
2019-11-01 00:37:16 -0400

    updating parser file

--- plotbot/framework/parser.py
+++ plotbot/framework/parser.py
@@ -2,12 +2,12 @@
 from os import path
 import pandas as pd
 
-input = "plot scatterplot dataset.csv xaxis yaxis"
+#input = "plot scatterplot dataset.csv xaxis yaxis"
+input = "plot scatterplot x1,x2,x3,x4 y1,y2,y3,y4"
 input = input.lower().split(" ")
 plots = ["scatterplot","barplot","boxplot"]
 
 def check_params(input):
-    input = input.split(" ")
     if (len(input) < 4):
         raise ValueError("Please provide all the required parameters for plotting graph.")
     elif (len(input) >= 6):
@@ -20,12 +20,11 @@ def check_params(input):
 
 #check the uploaded file
 def check_dataset_axis(input,loc):
-    input = input.lower().split(" ")
     df = pd.read_csv(loc)
     headers = list(df.head(1))
     headers = [each.lower() for each in headers]
-    x_axis = input[4].lower()
-    y_axis = input[4].lower()
+    x_axis = input[3]
+    y_axis = input[4]
     if x_axis not in headers:
         raise ValueError("Provided column for x-axis is not present in the dataset.Please provide valid column.")
     if y_axis not in headers:
@@ -34,16 +33,19 @@ def check_dataset_axis(input,loc):
 
 
 def check_plot_params(input):
-    input = input.lower().split(" ")
     if (len(input) == 5):
         file = input[2]
         loc = '../tmp/'+file
         if (path.exists(loc)):
             check_dataset_axis(input,loc)
         else:
-            raise ValueError("Please request by uploading the dataset. The mentioned dataset doesn't exist in our databse."
+            raise ValueError("Please request by uploading the dataset. The mentioned dataset doesn't exist in our database.")
     else:
+	    print("need to check uploaded file")
         ##check uploaded file
+        if ("," in input[2]) or (',' in input[3]) :
+            x_axis = input[2].split(",")
+            y_axis = input[2].split(",")
 
 
 
