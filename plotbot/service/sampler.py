da5621b4e76dc4efdf3f8c9ea241a54c741cb1aa
anbhatta <anbhatta@ncsu.edu>
2019-11-07 01:12:02 +0000

    Added smapler plot

--- plotbot/service/sampler.py
+++ plotbot/service/sampler.py
@@ -1,12 +1,12 @@
-def retrieve_snippet(type):
-    '''
+def retrieve_snippet(plottype):
+    
     snippet_dict={"scatterplot": ["scatterplot_code","scatterplot_graph"],
          "barplot": ["barplot_code","barplot_graph"], "boxplot": ["boxplot_code","boxplot_graph"]} 
     if plottype in snippet_dict:
         return snippet_dict[plottype]
     else:
-        raise ValueError('Requested plot sample not available')'''
-    pass
+        raise ValueError('Requested plot sample not available')
+    
 
 def fetch(input_txt):
     text_list = input_txt.lower().strip().split()
@@ -18,6 +18,7 @@ def fetch(input_txt):
     except:
         snippets = None
     #print(snippets)
+    print(snippets)
     if snippets is not None:
         for name in snippets:
             path="sample_plots/"+name+".png"
